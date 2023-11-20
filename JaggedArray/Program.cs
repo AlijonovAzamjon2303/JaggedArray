637b0d3f649872723a239bc4a766d0135789f1cf
alijo <alijo@ALIJONOV>
2023-11-20 11:10:20 +0500

    Add project files.

---
+++ JaggedArray/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        // this is multi-dimonsial array
+        int[,]a = new int[1, 2];
+
+        //this is jugged array
+        int[][] b = new int[1][];
+        b[0] = new int[2]; 
+    }
+}
